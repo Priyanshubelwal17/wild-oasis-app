0ea0f515dad846379b989fdb146b7deaf59dccd2
Priyanshubelwal <142991870+Priyanshubelwal17@users.noreply.github.com>
2025-07-07 19:02:55 +0530

    Learn styled-components props, CSS function, and 'as' prop

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,7 @@ import GlobalStyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
 import Input from "./ui/input";
 
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-`;
+import Heading from "./ui/Heading";
 
 const StyledApp = styled.div`
   background-color: orangered;
@@ -18,7 +15,9 @@ function App() {
     <>
       <GlobalStyles />
       <StyledApp>
-        <H1>Just for a while</H1>
+        <Heading as="h1">Just for a while</Heading>
+        <Heading as="h2">Just for a while</Heading>
+        <Heading as="h3">Just for a while</Heading>
         <Button onClick={() => alert("when i got to heaven")}>Dear</Button>
         <Button onClick={() => alert("summertimes madness")}>Lord</Button>
         <Input type="number" placeholder="Number of guests" />
---
+++ src/ui/Heading.jsx
@@ -0,0 +1,33 @@
+import styled, { css } from "styled-components";
+
+const alignCenter = css`
+  text-align: center;
+`;
+
+const Heading = styled.h1`
+  ${(props) =>
+    props.type === "h1" &&
+    css`
+      font-size: 3rem;
+    `}
+
+  ${(props) =>
+    props.type === "h2" &&
+    css`
+      font-size: 2rem;
+    `} 
+    
+    ${(props) =>
+    props.type === "h3" &&
+    css`
+      font-size: 1rem;
+      font-weight: 400;
+    `}
+    
+    line-height:1.4 /* font-size: 30px;
+  font-weight: 600;
+  background-color: var(--color-green-700);
+  ${alignCenter} */
+`;
+
+export default Heading;
