ee914bdc8faee05caf2db48d4ed90402e66ca36e
Priyanshubelwal <142991870+Priyanshubelwal17@users.noreply.github.com>
2025-07-07 18:23:26 +0530

    Learn how to add global styles and button input components

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,12 @@
 import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+
+import Button from "./ui/Button";
+import Input from "./ui/input";
 
 const H1 = styled.h1`
   font-size: 30px;
   font-weight: 600;
-`;
-
-const Button = styled.button`
-  font-size: 1.4rem;
-  padding: 1.2rem 1.6rem;
-  font-weight: 500;
-  border: none;
-  border-radius: 7px;
-  background-color: purple;
-  color: #ffffff;
-
-  cursor: pointer;
-
-  margin: 20px;
-`;
-
-const Input = styled.input`
-  border: 1px solid #ddd;
-  border-radius: 5px;
-  padding: 0.8rem 1.2rem;
 `;
 
 const StyledApp = styled.div`
@@ -31,12 +15,15 @@ const StyledApp = styled.div`
 `;
 function App() {
   return (
-    <StyledApp>
-      <H1>Just for a while</H1>
-      <Button onClick={() => alert("when i got to heaven")}>Dear</Button>
-      <Button onClick={() => alert("summertimes madness")}>Lord</Button>
-      <Input type="number" placeholder="Number of guests" />
-    </StyledApp>
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>Just for a while</H1>
+        <Button onClick={() => alert("when i got to heaven")}>Dear</Button>
+        <Button onClick={() => alert("summertimes madness")}>Lord</Button>
+        <Input type="number" placeholder="Number of guests" />
+      </StyledApp>
+    </>
   );
 }
 
