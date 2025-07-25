efd44adc7b72ee711c15afd42728e42bbde9764e
Priyanshubelwal <142991870+Priyanshubelwal17@users.noreply.github.com>
2025-07-25 19:04:35 +0530

    Add pagination prefetching

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,9 +1,11 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
 
 import { useSearchParams } from "react-router-dom";
+import { PAGE_SIZE } from "../../utils/constants";
 
 export function useBookings() {
+  const queryClient = useQueryClient();
   const [searchParams] = useSearchParams();
 
   //FILTER
@@ -21,6 +23,7 @@ export function useBookings() {
 
   const page = !searchParams.get("page") ? 1 : Number(searchParams.get("page"));
 
+  // Query
   const {
     data: { data: bookings, count } = {},
     isLoading,
@@ -30,6 +33,18 @@ export function useBookings() {
     queryFn: () => getBookings({ filter, sortBy, page }),
   });
 
+  // PRE-Fetching
+  const pageCount = Math.ceil(count / PAGE_SIZE);
+  if (page < pageCount)
+    queryClient.prefetchQuery({
+      queryKey: ["bookings", filter, sortBy, page + 1],
+      queryFn: () => getBookings({ filter, sortBy, page: page + 1 }),
+    });
+  if (page > 1)
+    queryClient.prefetchQuery({
+      queryKey: ["bookings", filter, sortBy, page - 1],
+      queryFn: () => getBookings({ filter, sortBy, page: page + 1 }),
+    });
   console.log(bookings);
 
   return { isLoading, error, bookings, count };
