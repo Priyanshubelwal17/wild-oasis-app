96eb7539de8f102eef14b508032178069ab25f6a
Priyanshubelwal <142991870+Priyanshubelwal17@users.noreply.github.com>
2025-07-30 14:05:37 +0530

    Implement ProtectedRoute, getCurrentUser, and useUser hook with React Query

---
+++ src/ui/ProtectedRoute.jsx
@@ -0,0 +1,16 @@
+import { useUser } from "../features/authentication/useUser";
+import Spinner from "./Spinner";
+
+function ProtectedRoute() {
+  // 1. Load the authenticated user
+  const { user, isLoading } = useUser();
+
+  //2. While loading, show a spinner
+  if (isLoading) return <Spinner />;
+
+  //3. If there is NO authenticated user, redirect to the login
+  // 4. If there IS a user, render the app
+  return children;
+}
+
+export default ProtectedRoute;
