e3679980abe19c8504d21005e4d542b60a3e0126
Priyanshubelwal <142991870+Priyanshubelwal17@users.noreply.github.com>
2025-08-02 23:59:32 +0530

    add context and provifer

--- src/ui/DarkModeToggle.jsx
+++ src/ui/DarkModeToggle.jsx
@@ -1,7 +1,9 @@
 import { HiOutlineMoon } from "react-icons/hi2";
 import ButtonIcon from "./ButtonIcon";
+import { useDarkMode } from "../context/DarkModeContext";
 
 export function DarkModeToggle() {
+  const { isDarkMode, toggleDarkMode } = useDarkMode();
   return (
     <ButtonIcon>
       <HiOutlineMoon />
